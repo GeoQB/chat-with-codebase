300fd4b766d6236936811d3972ece4020bb6ce1d
Mirko Kämpf <mirko.kaempf@gmail.com>
2024-04-12 15:11:04 +0200

    Ready for session 7

--- example-1/run.sh
+++ example-1/run.sh
@@ -7,10 +7,20 @@ source ~/.zsh    # ggf. auskommentieren
 #
 . /Users/mkaempf/opt/anaconda3/bin/activate && conda activate /Users/mkaempf/opt/anaconda3/envs/die-pa2;
 
-pip3 install -r requirements.txt
+#pip3 install -r requirements.txt
+
+#pip3 install -U langsmith
+
+export LANGCHAIN_TRACING_V2=true
+export LANGCHAIN_API_KEY=ls__d97f782cab824a37b02ee3bef05bc1bc
+
+# The below examples use the OpenAI API, though it's not necessary in general
+# export OPENAI_API_KEY=...
 
 # Use single PDF to ask questions ...
 #python3 01_RetrievalQA.py
 
 # Create the index ... with multiple files ...
 #python3 02_Docpool.py
+
+python3 03_Tracing.py
